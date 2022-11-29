3fc8330db7db5629172e732d13e60f7b2f17027d
Dhaker12 <dhaker.karous@esprit.tn>
2022-11-29 21:14:18 +0100

    Project setup

---
+++ src/pages/users/profiles.js
@@ -0,0 +1,6 @@
+
+import React from "react";
+import { faAngleDown, faAngleUp, faArrowDown, faArrowUp, faEdit, faEllipsisH, faExternalLinkAlt, faEye, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Col, Row, Nav, Card, Image, Button, Table, Dropdown, ProgressBar, Pagination, ButtonGroup } from '@themesberg/react-bootstrap';
+
+import axios from 'axios';
